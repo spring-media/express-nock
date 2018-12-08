646b82d2c5dc7801d0f6c36b4a784ed5474308ae
kommander <hasta84@gmail.com>
2018-12-08 16:09:40 +0100

    Resolve nock path, custom hash function, bump version

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const mkdirp = require('mkdirp');
 const fs = require('fs');
 const path = require('path');
 
-const defaultHashFn = (req) => crypto.createHash('md5').update(JSON.stringify(req.body)).digest('hex');
-
+const defaultHashFn = ({ url, query }) => crypto.createHash('md5').update(JSON.stringify({ url, query })).digest('hex');
+const resolveNockPath = (nockPath) => {
+  return path.isAbsolute(nockPath) ? nockPath : path.resolve(process.cwd(), nockPath);
+}
 const nockMiddleware = ({ nockPath, hashFn = defaultHashFn }) => (req, res, next) => {
+  const resolvedNockPath = resolveNockPath(nockPath);
+
   nock.recorder.rec({
     /* eslint-disable camelcase */
     dont_print: true,
@@ -27,10 +31,10 @@ const nockMiddleware = ({ nockPath, hashFn = defaultHashFn }) => (req, res, next
 
     const fileName = hashFn(req);
     const nockFileName = `${fileName}.json`;
-    const nockFilePath = path.join(nockPath, nockFileName);
+    const nockFilePath = path.join(resolvedNockPath, nockFileName);
 
-    if (!fs.existsSync(nockPath)) {
-      mkdirp.sync(nockPath);
+    if (!fs.existsSync(resolvedNockPath)) {
+      mkdirp.sync(resolvedNockPath);
     }
     fs.writeFileSync(nockFilePath, JSON.stringify(recording, null, 2));
   });
@@ -39,7 +43,8 @@ const nockMiddleware = ({ nockPath, hashFn = defaultHashFn }) => (req, res, next
 };
 
 const replayNocks = ({ nockPath }) => {
-  const dirFiles = fs.readdirSync(nockPath);
+  const resolvedNockPath = resolveNockPath(nockPath);
+  const dirFiles = fs.readdirSync(resolvedNockPath);
   const files = dirFiles.filter(file => (file.match(/\.json$/)));
 
   if (!nock.isActive()) {
@@ -47,7 +52,7 @@ const replayNocks = ({ nockPath }) => {
   }
 
   files.forEach(file => {
-    const scopes = nock.load(path.join(nockPath, file));
+    const scopes = nock.load(path.join(resolvedNockPath, file));
 
     scopes.forEach(scope => scope.persist());
   });
